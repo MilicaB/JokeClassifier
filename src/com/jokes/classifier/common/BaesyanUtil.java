da2becf7650ac7e660f2c95eb0499a6bc69fd1ef
yasen23 <yas.trifonov@gmail.com>
2016-02-13 03:27:58 +0200

    Use summation of logarithms in the classification.
    In this way we would avoid losing precision by multiplying a lot of small doubles.
    
    Also modified the default precision to rely on the size of the vocabulary.

--- src/com/jokes/classifier/common/BaesyanUtil.java
+++ src/com/jokes/classifier/common/BaesyanUtil.java
@@ -5,7 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 public class BaesyanUtil {
-	private static double DEFAULT_PROBABILITY = 0.001;
+	private final double defaultPositiveWordProbability;
+	private final double defaultNegativeWordProbability;
 
 	private final Vocabulary vocabulary;
 	private Map<String, Double> wordsProbabilityPos;
@@ -25,6 +26,11 @@ public class BaesyanUtil {
 		wordsCountPos = new HashMap<>();
 
 		processWordProbabilities(jokes);
+		defaultPositiveWordProbability = 1.0
+				/ (vocabulary.getAlphabet().size() + totalPositiveWords);
+		defaultNegativeWordProbability = 1.0
+				/ (vocabulary.getAlphabet().size() + totalNegativeWords);
+
 		getProbabilities(jokes);
 	}
 
@@ -78,8 +84,11 @@ public class BaesyanUtil {
 					/ (vocabularyCount + totalPositiveWords);
 			double wordProbNeg = (wordsCountNeg.getOrDefault(word, 0) + 1) * 1.0
 					/ (vocabularyCount + totalNegativeWords);
-			wordsProbabilityPos.put(word, wordProbPos);
-			wordsProbabilityNeg.put(word, wordProbNeg);
+
+			// HACK(yasen): Take natural logarithm in order to avoid multiplication of
+			// many small numbers which will tremendously influence the precision.
+			wordsProbabilityPos.put(word, Math.log(wordProbPos));
+			wordsProbabilityNeg.put(word, Math.log(wordProbNeg));
 		}
 	}
 	
@@ -106,27 +115,37 @@ public class BaesyanUtil {
 		return isGoodJoke;
 	}
 
-	// TODO(yasen): Use logarithm addition istead of simple multiplication (loses precision).
+	/**
+	 * Predicts a joke rating with a naive Bayes classification.
+	 * The calculation of the positive/negative rating uses is done with
+	 * summation of logarithms instead of product of small numbers in
+	 * order to increase precision.
+	 *
+	 * @param jokeText The text of the joke we want to classify.
+	 * @return negative if the joke is classified as bad, non-negative otherwise.
+	 */
 	public double predictJokeRating(String jokeText) {
 		String[] jokeWords = jokeText.split(ClassifiedJoke.DELIMITERS);
-		double positiveJokeProbability = positiveProbability;
-		double negativeJokeProbability = negativeProbability;
+		double positiveJokeProbability = Math.log(positiveProbability);
+		double negativeJokeProbability = Math.log(negativeProbability);
 		for (String word : jokeWords) {
 			if (vocabulary.withStemming()) {
 				word = vocabulary.stemWord(word);
 			}
 
 			if (wordsProbabilityPos.containsKey(word)) {
-				positiveJokeProbability *= wordsProbabilityPos.get(word);
+				positiveJokeProbability += wordsProbabilityPos.get(word);
 			} else {
-				positiveJokeProbability *= DEFAULT_PROBABILITY;
+				positiveJokeProbability += Math.log(defaultPositiveWordProbability);
 			}
 			if (wordsProbabilityNeg.containsKey(word)) {
-				negativeJokeProbability *= wordsProbabilityNeg.get(word);
+				negativeJokeProbability += wordsProbabilityNeg.get(word);
 			} else {
-				negativeJokeProbability *= DEFAULT_PROBABILITY;
+				negativeJokeProbability += Math.log(defaultNegativeWordProbability);
 			}
 		}
+		System.out.println("+ " + positiveJokeProbability);
+		System.out.println("- " + negativeJokeProbability);
 		return positiveJokeProbability - negativeJokeProbability;
 	}
 }
